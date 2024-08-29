fe40de367d16698ef767b629a6b1066b925ce3f3
Ravi Raj <raviroy2002@gmail.com>
2024-08-29 23:44:17 +0530

    Add files via upload

---
+++ merge_sort.java
@@ -0,0 +1,50 @@
+public class merge_sort {
+    public static void conquerer(int arr[], int mid, int si, int ei) {
+        int merged[] = new int[ei - si + 1];
+        int idx1 = si;
+        int idx2 = mid + 1;
+        int x = 0;
+        while (idx1 <= mid && idx2 <= ei) {
+            if (arr[idx1] <= arr[idx2]) {
+                merged[x] = arr[idx1];
+                x++;
+                idx1++;
+            } else {
+                merged[x++] = arr[idx2++];
+            }
+        }
+        while (idx1 <= mid) {
+            merged[x++] = arr[idx1++];
+        }
+        while (idx2 <= ei) {
+            merged[x++] = arr[idx2++];
+        }
+
+        for (int i = 0, j = si; i < merged.length; i++, j++) {
+            arr[j] = merged[i];
+
+        }
+
+    }
+
+    public static void divide(int arr[], int si, int ei) {
+        int mid = si + (ei - si) / 2;
+        if (si >= ei) {
+            return;
+        }
+
+        divide(arr, si, mid);
+        divide(arr, mid + 1, ei);
+        conquerer(arr, mid, si, ei);
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 6, 3, 9, 5, 2, 8 };
+        int n = arr.length;
+        divide(arr, 0, n - 1);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
